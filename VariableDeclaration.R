c494b6414a0a4c9e12a637eef536a12f01c92a71
Sajeban rex <antonyrex.2017472@iit.ac.lk>
2019-06-02 20:59:22 +0545

    Add existing file

--- VariableDeclaration.R
+++ VariableDeclaration.R
@@ -3,3 +3,34 @@
 greet <-"Hello"
 "Hi"-> greet2
 greet3 = "Whatsup"
+
+#vectors - Basic Data  Structures
+
+#Logical
+vtr1 = c(TRUE,FALSE)
+#Data type can be found by the following code
+class(vtr1)
+
+#numeric
+vtr2 =  c(3.14,3.12,4.556,4,2)
+class
+
+#integer represent with L
+vtr3 = c(2L,66L,87L)
+class(vtr3)
+
+#Everything will be changed into numeric format
+vtr4  =c(TRUE,35L,56.456)
+class(vtr4)
+vtr4
+
+#Everything will be changed to Character
+vtr5 = c("Hello",TRUE,45L,3.14)
+print(vtr5)
+
+#stores numbers from 1-5
+vtr6 = c(1:5)
+vtr6
+
+
+
